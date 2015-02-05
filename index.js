f39a89691a48e0bb651fd6bb73fce0db76f02bbc
Tom MacWright <tom@macwright.org>
2015-02-05 15:23:39 -0500

    Add category

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
 /**
-* Combines a {@link FeatureCollection} of {@link Point}, {@link LineString}, or {@link Polygon} features into {@link MultiPoint}, {@link MultiLineString}, or {@link MultiPolygon} features.
-*
-* @module turf/combine
-* @param {FeatureCollection} fc a FeatureCollection of any type
-* @return {FeatureCollection} a FeatureCollection of corresponding type to input
-* @example
-* var fc = {
-*   "type": "FeatureCollection",
-*   "features": [
-*     {
-*       "type": "Feature",
-*       "properties": {},
-*       "geometry": {
-*         "type": "Point",
-*         "coordinates": [19.026432, 47.49134]
-*       }
-*     }, {
-*       "type": "Feature",
-*       "properties": {},
-*       "geometry": {
-*         "type": "Point",
-*         "coordinates": [19.074497, 47.509548]
-*       }
-*     }
-*   ]
-* };
-*
-* var combined = turf.combine(fc);
-*
-* //=combined
-*/
+ * Combines a {@link FeatureCollection} of {@link Point}, {@link LineString}, or {@link Polygon} features into {@link MultiPoint}, {@link MultiLineString}, or {@link MultiPolygon} features.
+ *
+ * @module turf/combine
+ * @category misc
+ * @param {FeatureCollection} fc a FeatureCollection of any type
+ * @return {FeatureCollection} a FeatureCollection of corresponding type to input
+ * @example
+ * var fc = {
+ *   "type": "FeatureCollection",
+ *   "features": [
+ *     {
+ *       "type": "Feature",
+ *       "properties": {},
+ *       "geometry": {
+ *         "type": "Point",
+ *         "coordinates": [19.026432, 47.49134]
+ *       }
+ *     }, {
+ *       "type": "Feature",
+ *       "properties": {},
+ *       "geometry": {
+ *         "type": "Point",
+ *         "coordinates": [19.074497, 47.509548]
+ *       }
+ *     }
+ *   ]
+ * };
+ *
+ * var combined = turf.combine(fc);
+ *
+ * //=combined
+ */
 
 module.exports = function(fc) {
   var type = fc.features[0].geometry.type;
